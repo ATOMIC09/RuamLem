a20b04554e91e89ae7d029bf05724001a1ac2a9b
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2025-09-16 19:39:36 +0700

    Add filter (mock data)

--- src/app/components/searchbox.tsx
+++ src/app/components/searchbox.tsx
@@ -16,6 +16,30 @@ export default function SearchBox() {
                     <LuListFilter size={20} className="text-[#405168]" />
                 </button>
             </div>
+            {/* Filter menu */}
+            <div className="mt-2 text-sm text-[#5e7593] border rounded-3xl border-[#405168]">
+                <div className="px-4 py-2">
+                    รายวิชา
+                </div>
+                {/* Tag selection slider, hide slide bar */}
+                <div className="overflow-x-auto whitespace-nowrap px-4 py-2">
+                    {["คณิตศาสตร์", "ฟิสิกส์", "เคมี", "ชีวะ", "อังกฤษ", "สังคม", "ภาษาไทย", "อื่นๆ"].map((tag) => (
+                        <button key={tag} className="inline-block bg-white text-[#405168] px-3 py-1 mr-2 mb-2 rounded-full border border-[#405168] hover:bg-[#d1d5db] cursor-pointer transition-colors">
+                            {tag}
+                        </button>
+                    ))}
+                </div>
+                <div className="px-4 py-2">
+                    วันที่
+                </div>
+                <div className="overflow-x-auto whitespace-nowrap px-4 py-2">
+                    {["ล่าสุด", "วันนี้", "สัปดาห์นี้", "เดือนนี้", "ปีนี้"].map((tag) => (
+                        <button key={tag} className="inline-block bg-white text-[#405168] px-3 py-1 mr-2 mb-2 rounded-full border border-[#405168] hover:bg-[#d1d5db] cursor-pointer transition-colors">
+                            {tag}
+                        </button>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
