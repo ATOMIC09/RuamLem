a6ff4c559ed12232809ed2616e3508c138b6938f
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2025-10-08 13:08:53 +0700

    Fix hydration bug
    ซ่อนซะเลย

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,6 +22,7 @@ export default function RootLayout({
     <html lang="en">
       <body
         className={`${kanitFont.className} antialiased`}
+        suppressHydrationWarning
       >
         <Navbar />
         {children}
