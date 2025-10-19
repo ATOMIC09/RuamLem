3603d9663353d2d2aa6fbef05b9cdd24b09cc9d5
Phutawan Chanrueng <phutawanchanrueng@gmail.com>
2025-10-19 21:35:42 +0700

    Remove duplicate statistics route

--- src/routes/statistics-route.ts
+++
@@ -1,7 +0,0 @@
-import Elysia from "elysia";
-import { getStatisticsController } from "../controllers/statistics-controller";
-
-const statisticsRoute = new Elysia({ prefix: "/statistics" })
-  .get("/", () => getStatisticsController());
-
-export default statisticsRoute;
